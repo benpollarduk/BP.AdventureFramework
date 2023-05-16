0e156b3b09c3423f37f44b59484cf9c6325475ea
ben_pollard <ben@singerinstruments.com>
2023-05-16 16:30:11 +0100

    More map setup

--- BP.AdventureFramework/BP.AdventureFramework.Examples/Assets/Ship.cs
+++ BP.AdventureFramework/BP.AdventureFramework.Examples/Assets/Ship.cs
@@ -10,6 +10,10 @@ namespace BP.AdventureFramework.Examples.Assets
         private const string BridgeCentral = "Bridge (central)";
         private const string BridgePort = "Bridge (port)";
         private const string BridgeStarbord = "Bridge (starboard)";
+        private const string BridgeTunnel = "Tunnel";
+        private const string BridgeTunnelVertical = "Tunnel";
+        private const string BridgeTunnelEntry = "Tunnel entry";
+        private const string Hull = "Hull";
 
         internal static Region GenerateRegion(PlayableCharacter pC)
         {
@@ -18,6 +22,10 @@ namespace BP.AdventureFramework.Examples.Assets
             regionMaker[0, 0, 0] = new Room(BridgeCentral, "The central bridge is full of consoles filled with all kinds of dials, knobs and buttons.", new Exit(Direction.East), new Exit(Direction.West), new Exit(Direction.South));
             regionMaker[-1, 0, 0] = new Room(BridgePort, "The port side of the bridge is full of consoles filled with all kinds of dials, knobs and buttons.", new Exit(Direction.East));
             regionMaker[1, 0, 0] = new Room(BridgeStarbord, "The starboard side of the bridge is full of consoles filled with all kinds of dials, knobs and buttons.", new Exit(Direction.West));
+            regionMaker[0, -1, 0] = new Room(BridgeTunnel, "The tunnel leads up to the bridge.", new Exit(Direction.North), new Exit(Direction.Down));
+            regionMaker[0, -1, -1] = new Room(BridgeTunnelVertical, "The tunnel leads up to the bridge.", new Exit(Direction.Up), new Exit(Direction.Down));
+            regionMaker[0, -1, -2] = new Room(BridgeTunnelEntry, "The entry to the tunnel.", new Exit(Direction.Up), new Exit(Direction.South));
+            regionMaker[0, -2, -2] = new Room(Hull, "The underbelly of the ship.", new Exit(Direction.South));
 
             return regionMaker.Make(0, 0, 0);
         }

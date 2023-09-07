e97ecfad7be8190eafed14d7c71a867761764633
ben_pollard <ben@singerinstruments.com>
2023-09-07 12:49:20 +0100

    Removed examination results enumeration

--- BP.AdventureFramework/BP.AdventureFramework/Assets/ExaminationResult.cs
+++ BP.AdventureFramework/BP.AdventureFramework/Assets/ExaminationResult.cs
@@ -7,15 +7,6 @@ namespace BP.AdventureFramework.Assets
     /// </summary>
     public class ExaminationResult : Result
     {
-        #region Properties
-
-        /// <summary>
-        /// Get the type of result.
-        /// </summary>
-        public ExaminationResults Type { get; set; }
-
-        #endregion
-
         #region Methods
 
         /// <summary>
@@ -25,17 +16,6 @@ namespace BP.AdventureFramework.Assets
         public ExaminationResult(string description)
         {
             Description = description;
-            Type = ExaminationResults.DescriptionReturned;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the ExaminationResult class.
-        /// </summary>
-        /// <param name="description">A description of the result.</param>
-        /// <param name="type">The type of this result.</param>
-        public ExaminationResult(string description, ExaminationResults type) : this(description)
-        {
-            Type = type;
         }
 
         #endregion

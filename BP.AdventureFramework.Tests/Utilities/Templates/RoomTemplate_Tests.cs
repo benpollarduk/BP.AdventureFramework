e3e35262d1921c05029115e6ea9ca827985a1787
ben_pollard <ben@singerinstruments.com>
2024-01-02 10:35:36 +0000

    Added tests for templates

---
+++ BP.AdventureFramework.Tests/Utilities/Templates/RoomTemplate_Tests.cs
@@ -0,0 +1,36 @@
+﻿using BP.AdventureFramework.Assets.Locations;
+using BP.AdventureFramework.Utilities.Templates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BP.AdventureFramework.Tests.Utilities
+{
+    [TestClass]
+    public class RoomTemplate_Tests
+    {
+        private class ExampleRoom : RoomTemplate<ExampleRoom>
+        {
+            protected override Room OnCreate()
+            {
+                return new Room("", "");
+            }
+        }
+
+        [TestMethod]
+        public void GivenExample_WhenCreate_ThenNoExceptionThrown()
+        {
+            var pass = true;
+
+            try
+            {
+                ExampleRoom.Create();
+            }
+            catch (Exception)
+            {
+                pass = false;
+            }
+
+            Assert.IsTrue(pass);
+        }
+    }
+}

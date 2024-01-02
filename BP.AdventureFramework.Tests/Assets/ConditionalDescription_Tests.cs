a02d8c36ea8c0588d2a3941853b79935bb29f1d6
ben_pollard <ben@singerinstruments.com>
2024-01-02 11:05:21 +0000

    Tests for ConditionalDescription and InteractionResult

---
+++ BP.AdventureFramework.Tests/Assets/ConditionalDescription_Tests.cs
@@ -0,0 +1,27 @@
+﻿using BP.AdventureFramework.Assets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BP.AdventureFramework.Examples.Assets
+{
+    [TestClass]
+    public class ConditionalDescription_Tests
+    {
+        public void GivenGetDescription_WhenTrue_ThenReturnTrueDescription()
+        {
+            var conditional = new ConditionalDescription("A", "B", () => true);
+
+            var result = conditional.GetDescription();
+
+            Assert.AreEqual("A", result);
+        }
+
+        public void GivenGetDescription_WhenFalse_ThenReturnFalseDescription()
+        {
+            var conditional = new ConditionalDescription("A", "B", () => false);
+
+            var result = conditional.GetDescription();
+
+            Assert.AreEqual("B", result);
+        }
+    }
+}

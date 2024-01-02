323a0e3f54e034c70b768a348a19ef1b557a3a5b
ben_pollard <ben@singerinstruments.com>
2024-01-02 12:09:46 +0000

    Added tests for CompletionCheckResult and GameCommandInterpreter

---
+++ BP.AdventureFramework.Tests/Logic/CompletionCheckResult_Tests.cs
@@ -0,0 +1,18 @@
+﻿using BP.AdventureFramework.Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BP.AdventureFramework.Tests.Logic
+{
+    [TestClass]
+    public class CompletionCheckResult_Tests
+    {
+        public void GivenNotComplete_ThenIsCompletedFalseTitleEmptyDescriptionEmpty()
+        {
+            var result = CompletionCheckResult.NotComplete;
+
+            Assert.IsFalse(result.IsCompleted);
+            Assert.AreEqual(string.Empty, result.Title);
+            Assert.AreEqual(string.Empty, result.Description);
+        }
+    }
+}

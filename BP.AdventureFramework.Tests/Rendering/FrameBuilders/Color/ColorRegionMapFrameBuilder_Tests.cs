2ff959e1175bff7e4ef7cf6ec385f71c356c0947
ben_singer <ben@singerinstruments.com>
2024-02-08 15:49:21 +0000

    Added frame builder coverage

--- BP.AdventureFramework.Tests/Rendering/FrameBuilders/Color/ColorRegionMapFrameBuilder_Tests.cs
+++ BP.AdventureFramework.Tests/Rendering/FrameBuilders/Color/ColorRegionMapFrameBuilder_Tests.cs
@@ -22,5 +22,36 @@ namespace BP.AdventureFramework.Tests.Rendering.FrameBuilders.Color
 
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void GivenWidthOf80HeightOf50And2FloorsOnGroundFloor_WhenBuild_ThenNotNull()
+        {
+            var stringBuilder = new GridStringBuilder();
+            stringBuilder.Resize(new Size(80, 50));
+            var builder = new ColorRegionMapFrameBuilder(stringBuilder, new ColorRegionMapBuilder());
+            var region = new Region(string.Empty, string.Empty);
+            region.AddRoom(new Room(string.Empty, string.Empty, new Exit(Direction.Up)), 0, 0, 0);
+            region.AddRoom(new Room(string.Empty, string.Empty, new Exit(Direction.Up)), 0, 0, 1);
+
+            var result = builder.Build(region, 80, 50);
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void GivenWidthOf80HeightOf50And2FloorsOnSecondFloor_WhenBuild_ThenNotNull()
+        {
+            var stringBuilder = new GridStringBuilder();
+            stringBuilder.Resize(new Size(80, 50));
+            var builder = new ColorRegionMapFrameBuilder(stringBuilder, new ColorRegionMapBuilder());
+            var region = new Region(string.Empty, string.Empty);
+            region.AddRoom(new Room(string.Empty, string.Empty, new Exit(Direction.Up)), 0, 0, 0);
+            region.AddRoom(new Room(string.Empty, string.Empty, new Exit(Direction.Up)), 0, 0, 1);
+            region.SetStartRoom(0, 0, 1);
+
+            var result = builder.Build(region, 80, 50);
+
+            Assert.IsNotNull(result);
+        }
     }
 }

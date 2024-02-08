4199bef8df827cfcf2beec1df6dbfcde3888acf0
ben_singer <ben@singerinstruments.com>
2024-02-08 18:10:00 +0000

    Added game exceptions

--- BP.AdventureFramework.Tests/Logic/Game_Tests.cs
+++ BP.AdventureFramework.Tests/Logic/Game_Tests.cs
@@ -1,4 +1,6 @@
-﻿using BP.AdventureFramework.Assets;
+﻿using System.IO;
+using System.Linq;
+using BP.AdventureFramework.Assets;
 using BP.AdventureFramework.Assets.Characters;
 using BP.AdventureFramework.Assets.Locations;
 using BP.AdventureFramework.Logic;
@@ -128,6 +130,148 @@ namespace BP.AdventureFramework.Tests.Logic
             var result = game.GetAllPlayerVisibleExaminables();
 
             Assert.AreEqual(4, result.Length);
+        }
+
+        [TestMethod]
+        public void GivenSimpleGame_WhenDisplayTransition_ThenNoExceptionThrown()
+        {
+            var regionMaker = new RegionMaker(string.Empty, string.Empty);
+            var room = new Room(string.Empty, string.Empty);
+            regionMaker[0, 0, 0] = room;
+            var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(string.Empty, string.Empty, string.Empty, _ => overworldMaker.Make(), () => new PlayableCharacter(string.Empty, string.Empty), _ => EndCheckResult.NotEnded, _ => EndCheckResult.NotEnded).Invoke();
+
+            game.DisplayTransition("Test", "Test");
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void GivenSimpleGame_WhenDisplayAbout_ThenNoExceptionThrown()
+        {
+            var regionMaker = new RegionMaker(string.Empty, string.Empty);
+            var room = new Room(string.Empty, string.Empty);
+            regionMaker[0, 0, 0] = room;
+            var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(string.Empty, string.Empty, string.Empty, _ => overworldMaker.Make(), () => new PlayableCharacter(string.Empty, string.Empty), _ => EndCheckResult.NotEnded, _ => EndCheckResult.NotEnded).Invoke();
+
+            game.DisplayAbout();
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void GivenSimpleGame_WhenDisplayHelp_ThenNoExceptionThrown()
+        {
+            var regionMaker = new RegionMaker(string.Empty, string.Empty);
+            var room = new Room(string.Empty, string.Empty);
+            regionMaker[0, 0, 0] = room;
+            var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(string.Empty, string.Empty, string.Empty, _ => overworldMaker.Make(), () => new PlayableCharacter(string.Empty, string.Empty), _ => EndCheckResult.NotEnded, _ => EndCheckResult.NotEnded).Invoke();
+
+            game.DisplayHelp();
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void GivenSimpleGame_WhenDisplayMap_ThenNoExceptionThrown()
+        {
+            var regionMaker = new RegionMaker(string.Empty, string.Empty);
+            var room = new Room(string.Empty, string.Empty);
+            regionMaker[0, 0, 0] = room;
+            var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(string.Empty, string.Empty, string.Empty, _ => overworldMaker.Make(), () => new PlayableCharacter(string.Empty, string.Empty), _ => EndCheckResult.NotEnded, _ => EndCheckResult.NotEnded).Invoke();
+
+            game.DisplayMap();
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void GivenPlayer_WhenFindInteractionTarget_ThenReturnPlayer()
+        {
+            var regionMaker = new RegionMaker(string.Empty, string.Empty);
+            var room = new Room(string.Empty, string.Empty);
+            regionMaker[0, 0, 0] = room;
+            var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(string.Empty, string.Empty, string.Empty, _ => overworldMaker.Make(), () => new PlayableCharacter("Player", string.Empty), _ => EndCheckResult.NotEnded, _ => EndCheckResult.NotEnded).Invoke();
+
+            var result = game.FindInteractionTarget("Player");
+
+            Assert.AreEqual(game.Player, result);
+        }
+
+        [TestMethod]
+        public void GivenRoom_WhenFindInteractionTarget_ThenReturnRoom()
+        {
+            var regionMaker = new RegionMaker(string.Empty, string.Empty);
+            var room = new Room("Room", string.Empty);
+            regionMaker[0, 0, 0] = room;
+            var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(string.Empty, string.Empty, string.Empty, _ => overworldMaker.Make(), () => new PlayableCharacter(string.Empty, string.Empty), _ => EndCheckResult.NotEnded, _ => EndCheckResult.NotEnded).Invoke();
+
+            var result = game.FindInteractionTarget("Room");
+
+            Assert.AreEqual(game.Overworld.CurrentRegion.CurrentRoom, result);
+        }
+
+        [TestMethod]
+        public void GivenPlayerItem_WhenFindInteractionTarget_ThenReturnPlayerItem()
+        {
+            var regionMaker = new RegionMaker(string.Empty, string.Empty);
+            var room = new Room("Room", string.Empty);
+            regionMaker[0, 0, 0] = room;
+            var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(string.Empty, string.Empty, string.Empty, _ => overworldMaker.Make(), () => new PlayableCharacter(string.Empty, string.Empty, new Item("Item", string.Empty)), _ => EndCheckResult.NotEnded, _ => EndCheckResult.NotEnded).Invoke();
+
+            var result = game.FindInteractionTarget("Item");
+
+            Assert.AreEqual(game.Player.Items.First(), result);
+        }
+
+        [TestMethod]
+        public void GivenRoomItem_WhenFindInteractionTarget_ThenReturnRoomItem()
+        {
+            var regionMaker = new RegionMaker(string.Empty, string.Empty);
+            var room = new Room("Room", string.Empty);
+            room.AddItem(new Item("Item", string.Empty));
+            regionMaker[0, 0, 0] = room;
+            var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(string.Empty, string.Empty, string.Empty, _ => overworldMaker.Make(), () => new PlayableCharacter(string.Empty, string.Empty), _ => EndCheckResult.NotEnded, _ => EndCheckResult.NotEnded).Invoke();
+
+            var result = game.FindInteractionTarget("Item");
+
+            Assert.AreEqual(game.Overworld.CurrentRegion.CurrentRoom.Items.First(), result);
+        }
+
+        [TestMethod]
+        public void GivenUnknown_WhenFindInteractionTarget_ThenReturnNull()
+        {
+            var regionMaker = new RegionMaker(string.Empty, string.Empty);
+            var room = new Room("Room", string.Empty);
+            regionMaker[0, 0, 0] = room;
+            var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(string.Empty, string.Empty, string.Empty, _ => overworldMaker.Make(), () => new PlayableCharacter(string.Empty, string.Empty), _ => EndCheckResult.NotEnded, _ => EndCheckResult.NotEnded).Invoke();
+
+            var result = game.FindInteractionTarget("ABC");
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void GivenSimpleGame_WhenExecuteWithNoConsoleAccess_ThenIOExceptionThrown()
+        {
+            Assert.ThrowsException<IOException>(() =>
+            {
+                var regionMaker = new RegionMaker(string.Empty, string.Empty);
+                var room = new Room("Room", string.Empty);
+                regionMaker[0, 0, 0] = room;
+                var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
+                var game = Game.Create(string.Empty, string.Empty, string.Empty, _ => overworldMaker.Make(), () => new PlayableCharacter(string.Empty, string.Empty), _ => EndCheckResult.NotEnded, _ => EndCheckResult.NotEnded);
+
+                Game.Execute(game);
+            });
         }
     }
 }
